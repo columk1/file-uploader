fb65efd81c75aed0c5a4a06996e39b45913492b6
Colum Kelly <columk1@users.noreply.github.com>
2024-08-02 00:41:09 -0700

    Create module for local strategy

---
+++ auth/strategies/local.ts
@@ -0,0 +1,23 @@
+import { Strategy as LocalStrategy } from 'passport-local'
+import prisma from '../../prisma/prisma'
+import bcrypt from 'bcrypt'
+
+export default new LocalStrategy(async (username, password, done) => {
+  console.log('New Local Strategy')
+  try {
+    const user = await prisma.user.findFirst({ where: { username } })
+    if (!user) {
+      console.log('Incorrect username')
+      return done(null, false, { message: 'Username not found' })
+    }
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      console.log('Incorrect password')
+      return done(null, false, { message: 'Incorrect password' })
+    }
+    console.log('Found user')
+    return done(null, user)
+  } catch (err) {
+    return done(err)
+  }
+})
