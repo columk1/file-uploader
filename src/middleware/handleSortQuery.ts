8e9558d664da7151bf3b504b6deedb6a58d24c3e
Colum Kelly <columk1@users.noreply.github.com>
2024-08-14 22:17:56 -0700

    Create middleware to add sort query to the request

---
+++ src/middleware/handleSortQuery.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express'
+import { Prisma } from '@prisma/client'
+
+export default function handleSortQuery(req: Request, res: Response, next: NextFunction) {
+  const defaultSort = [{ type: 'asc' as 'asc' | 'desc' }]
+  const sortQuery = req.query.sort
+
+  if (!sortQuery) {
+    req.sortCriteria = defaultSort
+    return next()
+  }
+  let sortCriteria: Prisma.EntityOrderByWithRelationInput[] = defaultSort
+
+  if (typeof sortQuery === 'string') {
+    const getSortCriteria = (searchParams: string) =>
+      searchParams.split(',').map((item) => {
+        const direction = item.startsWith('-') ? 'desc' : 'asc'
+        const field = direction === 'desc' ? item.slice(1) : item
+        return { [field]: direction }
+      })
+    const additionalSort = getSortCriteria(sortQuery)
+    sortCriteria.push(...additionalSort)
+  }
+
+  req.sortCriteria = sortCriteria
+  next()
+}
