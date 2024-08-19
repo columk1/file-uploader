30d7e086988f73084e1d9330ce65231cd9266e25
Colum Kelly <columk1@users.noreply.github.com>
2024-08-19 14:06:46 -0700

    Add express.json

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '../public')))
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(morgan('dev'))
 app.use(sessionConfig)
 
