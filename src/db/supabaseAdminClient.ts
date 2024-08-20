4ad4599d0f175a1e3c7b0ace7d9471de0e86e007
Colum Kelly <columk1@users.noreply.github.com>
2024-08-19 22:46:37 -0700

    Use service role instead of anon on server

--- src/db/supabaseAdminClient.ts
+++ src/db/supabaseAdminClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://ulgkocqdombbzvyucicr.supabase.co'
-const SUPABASE_KEY = process.env.SUPABASE_KEY || ''
+const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 
 const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_KEY)
 
