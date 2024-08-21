216e9c46c78bc52a6ca51567f939939770cbc16d
Colum Kelly <columk1@users.noreply.github.com>
2024-08-21 00:00:02 -0700

    Refactor router and controller using new dirService and middleware

--- src/controllers/entityController.ts
+++ src/controllers/entityController.ts
@@ -1,23 +1,33 @@
 import { NextFunction, Request, Response } from 'express'
-import { getAllFilenames, getFolderTree, getPathSegments } from 'src/services/dirService'
-import prisma from 'src/db/prismaClient'
+import {
+  createFile,
+  createFolder,
+  createSharedFolder,
+  deleteEntityById,
+  getAllFilenames,
+  getFolderTree,
+  getPathSegments,
+  getFolderContents,
+  isChildOf,
+  getFilename,
+} from 'src/services/dirService'
 import helpers from 'src/lib/utils/ejsHelpers'
 import supabaseAdmin from 'src/db/supabaseAdminClient'
 import { Readable } from 'stream'
 import { defaultError, defaultErrorQuery } from 'src/lib/utils/errorMessages'
+import { getUserEntities, getFolderEntityById } from 'src/services/dirService'
 
 // GET: /
 const getDashboard = async (req: Request, res: Response) => {
+  const userId = req.user?.id
+  if (!userId) return res.status(401).send({ errors: [{ message: defaultError }] })
+
   const { sortCriteria } = req
 
-  const files = await prisma.entity.findMany({
-    where: { userId: req.user?.id, parentId: null },
-    orderBy: sortCriteria,
-  })
+  const files = await getUserEntities(userId, sortCriteria)
 
   const sortQuery = sortCriteria?.reduce((acc, curr) => ({ ...acc, ...curr }), {})
   const folders = await getFolderTree(req.user?.id, null)
-  console.log({ folders })
 
   res.render('dashboard', {
     title: 'File Uploader',
@@ -32,21 +42,14 @@ const getDashboard = async (req: Request, res: Response) => {
   })
 }
 
-// GET: /:entityId (Same view as Dashboard)
-const getEntityById = async (req: Request, res: Response) => {
+// GET: /:folderId (Same view as Dashboard)
+const getFolder = async (req: Request, res: Response) => {
   const id = Number(req.params.entityId)
   if (!id) return res.redirect('/')
 
   const { sortCriteria } = req
 
-  const entity = await prisma.entity.findUnique({
-    where: { id },
-    include: {
-      childEntities: {
-        orderBy: sortCriteria,
-      },
-    },
-  })
+  const entity = await getFolderEntityById(id, sortCriteria)
   if (!entity) return res.status(404).send('Not found')
 
   const { name, type, childEntities: files, parentId } = entity
@@ -71,21 +74,15 @@ const getEntityById = async (req: Request, res: Response) => {
 }
 
 // POST: /new Create a new folder
-const createFolder = async (req: Request, res: Response) => {
+const handleCreateFolder = async (req: Request, res: Response) => {
   {
-    const id = req.user?.id
-    if (!id) return res.status(401).send({ errors: [{ message: defaultError }] })
+    const userId = req.user?.id
+    if (!userId) return res.status(401).send({ errors: [{ message: defaultError }] })
 
     const parentId = Number(req.body.parentId) || null
+    const name = req.body.name || 'New Folder'
 
-    const newFolder = await prisma.entity.create({
-      data: {
-        type: 'FOLDER',
-        name: req.body.name || Date.now().toString(),
-        userId: id,
-        parentId,
-      },
-    })
+    const newFolder = await createFolder(userId, parentId, name)
     res.redirect(`back`)
   }
 }
@@ -95,7 +92,6 @@ const getUploadUrl = async (req: Request, res: Response) => {
   const id = req.user?.id
   if (!id) return res.status(401).send({ errors: [{ message: defaultError }] })
 
-  console.log('Request body: ', req.body)
   const filename = req.body.filename
   const bucketName = 'files'
   const filePath = `${id}/${filename}`
@@ -113,62 +109,53 @@ const getUploadUrl = async (req: Request, res: Response) => {
 
 // POST: /upload Upload a file
 const uploadFile = async (req: Request, res: Response) => {
-  {
-    try {
-      const id = req.user?.id
-      const file = req.file
-      if (!id) return res.status(401).send({ errors: [{ message: defaultError }] })
+  try {
+    const userId = req.user?.id
+    if (!userId) return res.status(401).send({ errors: [{ message: defaultError }] })
 
-      // req.file is the name of the user's file in the form, 'uploaded_file'
-      if (!file) return res.status(400).send({ errors: [{ message: 'Bad request' }] })
-      const { originalname, mimetype, size, buffer } = file
-      const parentId = Number(req.body.parentId) || null
+    // req.file is the name of the user's file in the form, 'uploaded_file'
+    const file = req.file
+    if (!file) return res.status(400).send({ errors: [{ message: 'Bad request' }] })
 
-      const bucketName = 'files'
-      const options = {
-        contentType: mimetype,
-        upsert: false,
-        duplex: 'half', // allows binary stream, otherwise must convert: decode(buffer.toString('base64')
-      }
-      const filePath = `${id}/${originalname}`
+    const { originalname, mimetype, size, buffer } = file
+    const parentId = Number(req.body.parentId) || null
 
-      const bufferStream = new Readable()
-      bufferStream.push(buffer)
-      bufferStream.push(null) // end of stream
-
-      const { data, error } = await supabaseAdmin.storage
-        .from(bucketName)
-        .upload(filePath, bufferStream, options)
-
-      if (error) {
-        console.log(error)
-        if ('statusCode' in error) {
-          if (error.statusCode === '409') {
-            // duplicate file
-            return res.redirect(`/${parentId}?error=${encodeURIComponent(error.message)}`)
-          } else if (error.statusCode === '413') {
-            // file size limit exceeded
-            return res.redirect(`/${parentId}?error=${encodeURIComponent(error.message)}`)
-          }
-        }
-        return res.redirect(`/${parentId}?error=${defaultErrorQuery}`)
-      }
-
-      // add to database using prisma
-      const newFile = await prisma.entity.create({
-        data: {
-          type: 'FILE',
-          name: originalname,
-          mimeType: mimetype,
-          size,
-          userId: id,
-          parentId,
-        },
-      })
-      res.redirect('/' + parentId)
-    } catch (error) {
-      console.log(error)
+    const bucketName = 'files'
+    const options = {
+      contentType: mimetype,
+      upsert: false,
+      duplex: 'half', // allows binary stream, otherwise must convert: decode(buffer.toString('base64')
     }
+    const filePath = `${userId}/${originalname}`
+
+    const bufferStream = new Readable()
+    bufferStream.push(buffer)
+    bufferStream.push(null) // end of stream
+
+    const { data, error } = await supabaseAdmin.storage
+      .from(bucketName)
+      .upload(filePath, bufferStream, options)
+
+    if (error) {
+      console.log(error)
+      if ('statusCode' in error) {
+        if (error.statusCode === '409') {
+          // duplicate file
+          return res.redirect(`/${parentId}?error=${encodeURIComponent(error.message)}`)
+        } else if (error.statusCode === '413') {
+          // file size limit exceeded
+          return res.redirect(`/${parentId}?error=${encodeURIComponent(error.message)}`)
+        }
+      }
+      return res.redirect(`/${parentId}?error=${defaultErrorQuery}`)
+    }
+
+    // add to database
+    await createFile(originalname, mimetype, size, userId, parentId)
+
+    res.redirect('/' + parentId)
+  } catch (error) {
+    console.log(error)
   }
 }
 
@@ -182,22 +169,14 @@ const deleteEntity = async (req: Request, res: Response, next: NextFunction) => 
 
     const { type, parentId } = req.body
 
-    // TODO: Clean up this section
-    let filenames = []
+    const filenames =
+      type === 'FOLDER'
+        ? await getAllFilenames(userId, id) // recursively get all filenames
+        : [await getFilename(id)]
 
-    if (type === 'FOLDER') {
-      filenames = await getAllFilenames(userId, id) // recursively get all filenames
-    } else {
-      filenames = await prisma.entity.findUnique({ where: { id } }).then((entity) => [entity?.name])
-    }
+    await deleteEntityById(id)
 
-    const deletedEntity = await prisma.entity.delete({
-      where: {
-        id,
-      },
-    })
-
-    return res.redirect(`/${parentId}?success=${encodeURIComponent('Deleted Successfully')}`)
+    res.redirect(`/${parentId}?success=${encodeURIComponent('Deleted Successfully')}`)
 
     // Continue to remove from storage
     filenames.forEach(async (filename) => {
@@ -214,7 +193,7 @@ const deleteEntity = async (req: Request, res: Response, next: NextFunction) => 
   }
 }
 
-// GET: /download/:entityId // TODO: Find out if params should be used here or not
+// GET: /download/:entityId // TODO: could use filename as params instead of query
 const downloadFile = async (req: Request, res: Response) => {
   try {
     const fileName = req.query.name
@@ -229,31 +208,6 @@ const downloadFile = async (req: Request, res: Response) => {
     } else {
       console.log('Error fetching signed URL')
       res.redirect(`/${parentId}?error=${defaultErrorQuery}`)
-    }
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-// GET: /public/download/:entityId
-const downloadPublicFile = async (req: Request, res: Response) => {
-  try {
-    const id = Number(req.params.entityId)
-    const { filename } = req.query
-
-    const userId = await prisma.entity
-      .findUnique({ where: { id } })
-      .then((entity) => entity?.userId)
-
-    const filePath = `${userId}/${filename}`
-    const { data } = await supabaseAdmin.storage
-      .from('files')
-      .createSignedUrl(filePath, 60, { download: true })
-
-    if (data?.signedUrl) {
-      res.redirect(data.signedUrl)
-    } else {
-      res.redirect(`/${req.originalUrl}?error=${defaultErrorQuery}`)
     }
   } catch (err) {
     console.log(err)
@@ -285,18 +239,12 @@ const shareFolder = async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.user?.id
     if (!userId) return res.status(401).send({ errors: [{ message: 'Unauthorized' }] })
 
-    const id = Number(req.params.entityId)
+    const folderId = Number(req.params.entityId)
     const defaultDuration = 60 * 60 * 24 * 3 * 1000 // 3 days
     const durationMS = Number(req.query.h) * 60 * 60 * 1000 || defaultDuration
     const expiresAt = new Date(Date.now() + durationMS)
 
-    const newSharedFolder = await prisma.sharedFolder.create({
-      data: {
-        userId,
-        folderId: id,
-        expiresAt,
-      },
-    })
+    const newSharedFolder = await createSharedFolder(userId, folderId, expiresAt)
     if (!newSharedFolder) {
       return res.status(500).json({ error: defaultError })
     }
@@ -308,40 +256,24 @@ const shareFolder = async (req: Request, res: Response, next: NextFunction) => {
   }
 }
 
-// GET: /public/:entityId Or unique publicSessionId?
+// GET: /public/:sharedFolderId // TODO:/:entityId instead of query params
 const getPublicFolder = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const id = req.params.sharedFolderId
-    if (!id) return res.redirect('/')
+    const { sortCriteria, sharedFolder } = req
+    if (!sharedFolder) return res.status(404).send('Not found')
 
-    const { sortCriteria } = req
+    const path = Number(req.query.path) // TODO: improve name, target/targetFolderId?
 
-    const sharedFolder = await prisma.sharedFolder.findUnique({
-      where: { id },
-      include: { folder: true },
-    })
-    if (!sharedFolder) {
-      return res.status(404).send({ errors: [{ message: 'Not found' }] })
-    }
-
-    if (new Date() > sharedFolder.expiresAt) {
-      console.log('Link has expired')
-      return res.status(404).send('Not found')
-    }
-
-    const userId = sharedFolder.userId
-    const path = Number(req.query.path)
-    const rootFolder = { id: sharedFolder.folderId, name: sharedFolder.folder.name }
-    let currentFolderId = rootFolder.id
     if (path) {
-      const currentFolderEntry = await prisma.entity.findUnique({ where: { id: path } })
-      currentFolderEntry && (currentFolderId = currentFolderEntry.id)
+      const isValidPath = await isChildOf(sharedFolder.folderId, path)
+      if (!isValidPath) return res.status(404).send('Not found')
     }
 
-    const files = await prisma.entity.findMany({
-      where: { userId, parentId: currentFolderId },
-      orderBy: sortCriteria,
-    })
+    const rootFolder = { id: sharedFolder.folderId, name: sharedFolder.folder.name }
+    const currentFolderId = path ? path : rootFolder.id
+
+    const files = await getFolderContents(currentFolderId, sortCriteria)
     if (!files) return res.status(404).send('Not found')
 
     const folders = await getFolderTree(4, rootFolder.id)
@@ -365,15 +297,40 @@ const getPublicFolder = async (req: Request, res: Response, next: NextFunction) 
   }
 }
 
+// GET: /public/:sharedFolderId/download/:entityId
+const handleSharedFileDownload = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { sharedFolder } = req
+    const { filename } = req.query
+
+    // Get userId of file owner (could get from earlier query)
+    const userId = sharedFolder.userId
+
+    const filePath = `${userId}/${filename}`
+    const { data } = await supabaseAdmin.storage
+      .from('files')
+      .createSignedUrl(filePath, 60, { download: true })
+
+    if (data?.signedUrl) {
+      res.redirect(data.signedUrl)
+    } else {
+      res.redirect(`/${req.originalUrl}?error=${defaultErrorQuery}`)
+    }
+  } catch (err) {
+    console.log(err)
+    next(err)
+  }
+}
+
 export {
   getDashboard,
-  getEntityById,
-  createFolder,
+  getFolder,
+  handleCreateFolder,
   getUploadUrl,
   uploadFile,
   deleteEntity,
   downloadFile,
-  downloadPublicFile,
+  handleSharedFileDownload,
   shareFile,
   shareFolder,
   getPublicFolder,
--- src/routers/entityRouter.ts
+++ src/routers/entityRouter.ts
@@ -1,19 +1,19 @@
 import { Router } from 'express'
 import {
   getDashboard,
-  getEntityById,
+  getFolder,
   uploadFile,
   deleteEntity,
   downloadFile,
   shareFile,
-  createFolder,
+  handleCreateFolder,
   shareFolder,
   getPublicFolder,
-  downloadPublicFile,
+  handleSharedFileDownload,
 } from 'src/controllers/entityController'
 import { isAuthenticated } from 'src/middleware/isAuthenticated'
 import { handleSortQuery } from 'src/middleware/handleSortQuery'
-import { checkPublicAccess } from 'src/middleware/checkPublicAccess'
+import { validateSharedFolder } from 'src/middleware/validateSharedFolder'
 import multer from 'multer'
 
 const upload = multer({ storage: multer.memoryStorage() })
@@ -21,14 +21,18 @@ const upload = multer({ storage: multer.memoryStorage() })
 const router = Router()
 
 router.get('/', isAuthenticated, handleSortQuery, getDashboard)
-router.get('/:entityId', isAuthenticated, handleSortQuery, getEntityById)
-router.post('/new', isAuthenticated, createFolder)
+router.get('/:entityId', isAuthenticated, handleSortQuery, getFolder)
+router.post('/new', isAuthenticated, handleCreateFolder)
 router.post('/upload', isAuthenticated, upload.single('uploaded_file'), uploadFile)
 router.post('/delete/:entityId', isAuthenticated, deleteEntity)
 router.get('/download/:entityId', isAuthenticated, downloadFile)
 router.get('/share/file/:fileName', isAuthenticated, shareFile)
 router.get('/share/folder/:entityId', isAuthenticated, shareFolder)
-router.get('/public/:sharedFolderId', handleSortQuery, getPublicFolder)
-router.get('/public/download/:entityId', checkPublicAccess, downloadPublicFile)
+router.get('/public/:sharedFolderId', validateSharedFolder, handleSortQuery, getPublicFolder)
+router.get(
+  '/public/:sharedFolderId/download/:entityId',
+  validateSharedFolder,
+  handleSharedFileDownload
+)
 
 export default router
