b4ecb83f96ebadcc42545117a0d1495265eebd69
Colum Kelly <columk1@users.noreply.github.com>
2024-08-24 11:14:11 -0700

    Create generic error handler

---
+++ src/lib/utils/handleError.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express'
+
+export const handleError = (err: Error, req: Request, res: Response) => {
+  console.error(err)
+
+  const error = req.app.get('env') === 'development' ? err : {}
+  res.status(err.status || 500)
+  res.render('error', { error })
+}
