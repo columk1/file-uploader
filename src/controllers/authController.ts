46f64fcdfa87bdc89f4bd81f8d3286655427d457
Colum Kelly <columk1@users.noreply.github.com>
2024-08-25 16:39:01 -0700

    Refactor auth controller, remove async handler

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,93 +1,100 @@
 import asyncHandler from 'express-async-handler'
+import { NextFunction, Request, Response } from 'express'
 import passport from 'src/auth/passportConfig'
-import prisma from 'src/db/prismaClient'
 import bcrypt from 'bcrypt'
-import { z } from 'zod'
 import { SignUpSchema } from 'src/models/schemas'
+import { createUser, findUserByUsername } from 'src/repositories/users.repository'
+import { Prisma } from '@prisma/client'
 
-export const login = asyncHandler(async (req, res, next) => {
-  // returns a middleware function which is immediately invoked with (req, res, next)
-  passport.authenticate('local', {
-    failureMessage: true,
-    successRedirect: '/',
-    failureRedirect: '/login',
-  })(req, res, next)
-})
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // returns a middleware function which is immediately invoked with (req, res, next)
+    passport.authenticate('local', {
+      failureMessage: true,
+      successRedirect: '/',
+      failureRedirect: '/login',
+    })(req, res, next)
+  } catch (err) {
+    next(err)
+  }
+}
 
 // Todo: create type for possible error messages
-
-export const renderLogin = asyncHandler(async (req, res) => {
-  if (req.isAuthenticated()) return res.redirect('/')
-  let usernameError, passwordError
-  const { messages } = req.session
-  if (messages) {
-    if (messages[0].includes('Username')) {
-      usernameError = messages[0]
-    } else {
-      if (messages[0].includes('password')) {
-        passwordError = messages[0]
+export const renderLogin = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (req.isAuthenticated()) return res.redirect('/')
+    let usernameError, passwordError
+    const { messages } = req.session
+    if (messages) {
+      if (messages[0].includes('Username')) {
+        usernameError = messages[0]
       } else {
-        usernameError = 'Invalid credentials'
+        if (messages[0].includes('password')) {
+          passwordError = messages[0]
+        } else {
+          usernameError = 'Invalid credentials'
+        }
       }
     }
+    req.session.messages = undefined
+    res.render('login', { title: 'Login', usernameError, passwordError })
+  } catch (err) {
+    next(err)
   }
-  req.session.messages = undefined
-  res.render('login', { title: 'Login', usernameError, passwordError })
-})
+}
 
-export const logout = asyncHandler(async (req, res, next) => {
+export const logout = async (req: Request, res: Response, next: NextFunction) =>
   req.logout((err) => (err ? next(err) : res.redirect('/')))
-})
 
-export const signup = asyncHandler(async (req, res, next) => {
+export const signup = async (req: Request, res: Response, next: NextFunction) => {
   const defaultErrorMessage =
     'Validation failed. Please ensure all fields are filled out correctly.'
-
-  if (!req.body.username || !req.body.password) {
-    return res.status(400).render('sign-up', { title: 'Sign Up', error: defaultErrorMessage })
-  }
-
-  const validatedFields = SignUpSchema.safeParse(req.body)
-  if (!validatedFields.success) {
-    console.log(validatedFields.error.flatten().fieldErrors)
-    return res.status(400).render('sign-up', { title: 'Sign Up', error: defaultErrorMessage })
-  }
-  const { username, password } = validatedFields.data
-
-  const hashedPassword = await bcrypt.hash(password, 10)
-
   try {
-    await prisma.user.create({
-      data: {
-        username,
-        password: hashedPassword,
-      },
-    })
-  } catch (err: any) {
-    let message = 'Internal Error'
-    if (err?.code === 'P2002') {
-      message = 'An account with that username already exists'
+    const validationResult = SignUpSchema.safeParse(req.body)
+    if (!validationResult.success) {
+      const errors = validationResult.error.flatten().fieldErrors
+      return res.status(400).render('sign-up', {
+        title: 'Sign Up',
+        usernameError: errors.username ? errors.username[0] : null,
+        passwordError: errors.password ? errors.password[0] : null,
+        error: defaultErrorMessage,
+      })
     }
-    return res.status(400).render('sign-up', { title: 'Sign Up', usernameError: message })
-  }
-  res.redirect('/')
-})
 
-export const renderSignup = asyncHandler(async (req, res) => {
-  if (req.isAuthenticated()) return res.redirect('/')
-  res.render('sign-up', { title: 'Sign Up' })
-})
+    const { username, password } = validationResult.data
 
-export const validateUniqueUsername = asyncHandler(async (req, res, next) => {
-  const username = req.query.username
-  if (!username || typeof username !== 'string') {
-    res.status(401).json({ isAvailable: false })
-    return next()
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    await createUser(username, hashedPassword)
+
+    res.redirect('/')
+  } catch (err) {
+    let errorMessage = 'Internal Error'
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      errorMessage = 'An account with that username already exists'
+    }
+    return res.status(400).render('sign-up', { title: 'Sign Up', usernameError: errorMessage })
   }
-  const user = await prisma.user.findUnique({
-    where: {
-      username,
-    },
-  })
-  res.status(200).json({ isAvailable: !user })
-})
+}
+
+export const renderSignup = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (req.isAuthenticated()) return res.redirect('/')
+    res.render('sign-up', { title: 'Sign Up' })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const validateUniqueUsername = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const username = req.query.username
+    if (!username || typeof username !== 'string') {
+      return res.status(401).json({ isAvailable: false })
+    }
+    const user = await findUserByUsername(username)
+    res.status(200).json({ isAvailable: !user })
+  } catch (err) {
+    next(err)
+  }
+}
