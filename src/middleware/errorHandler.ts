1efa53c3b82f2b24cc66b89b6b01e99b1b4e496b
Colum Kelly <columk1@users.noreply.github.com>
2024-08-29 12:36:46 -0700

    Clean up error middleware

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,4 +1,13 @@
 import type { ErrorRequestHandler } from 'express'
-import { handleError } from '@/lib/utils/handleError'
 
-export const errorHandler: ErrorRequestHandler = (err, req, res, next) => handleError(err, req, res)
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err)
+
+  const error =
+    req.app.get('env') === 'production'
+      ? err
+      : { status: 500, message: 'Oops! Something went wrong.' }
+
+  res.status(err.status || 500)
+  res.render('error', { error })
+}
