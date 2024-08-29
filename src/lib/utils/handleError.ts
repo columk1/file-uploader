4fad817bc4fef60f6213aba7ea0c40fab620a1b5
Colum Kelly <columk1@users.noreply.github.com>
2024-08-29 10:59:42 -0700

    Update default error page

--- src/lib/utils/handleError.ts
+++ src/lib/utils/handleError.ts
@@ -3,7 +3,10 @@ import type { Request, Response } from 'express'
 export const handleError = (err: Error, req: Request, res: Response) => {
   console.error(err)
 
-  const error = req.app.get('env') === 'development' ? err : {}
+  const error =
+    req.app.get('env') === 'production'
+      ? err
+      : { status: 500, message: 'Oops! Something went wrong.' }
   res.status(err.status || 500)
   res.render('error', { error })
 }
