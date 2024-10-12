f07d216c0a9ac9418fb6a58e10f4f73c17bfea20
Colum Kelly <columk1@users.noreply.github.com>
2024-10-12 12:17:12 -0700

    Remove unused copy config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,5 @@
-import path from 'node:path'
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import copy from 'rollup-plugin-copy'
 import css from 'rollup-plugin-css-only'
 import terser from '@rollup/plugin-terser'
 
@@ -27,19 +25,26 @@ export default {
     nodeResolve(),
     commonjs(),
     css({ output: 'stylesheets/bundle.css' }),
-    copy({
-      targets: [
-        // Copies all icons from shoelace to the public folder, done manually to save space
-        // {
-        //   src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets'),
-        //   dest: path.resolve(__dirname, 'public/vendors/shoelace'),
-        // },
-        {
-          src: path.resolve(__dirname, 'src/assets'),
-          dest: path.resolve(__dirname, 'public'),
-        },
-      ],
-    }),
     terser(), // minify js
   ],
 }
+
+// Old copy config
+
+// import path from 'node:path'
+// import copy from 'rollup-plugin-copy'
+
+// This is now done manually to reduce bundle size
+// copy({
+//   targets: [
+//     // Copies all icons from shoelace to the public folder, done manually to save space
+//     // {
+//     //   src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets'),
+//     //   dest: path.resolve(__dirname, 'public/vendors/shoelace'),
+//     // },
+//     {
+//       src: path.resolve(__dirname, 'src/assets'),
+//       dest: path.resolve(__dirname, 'public'),
+//     },
+//   ],
+// }),
